d064fe2735f0fb3a9a4beb8d0ea4f9b6573b0c8e
Indrajeetsinh Chauhan <indrajeet.chauhan@rakuten.com>
2024-07-04 17:01:13 +0900

    Adding bottom sheet layout code

--- app/src/main/java/com/indrajeet/chauhan/materialuitutorial/MainActivity.kt
+++ app/src/main/java/com/indrajeet/chauhan/materialuitutorial/MainActivity.kt
@@ -2,20 +2,10 @@ package com.indrajeet.chauhan.materialuitutorial
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.indrajeet.chauhan.materialuitutorial.ui.theme.MaterialUITutorialTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView()
+        setContentView(R.layout.activity_main)
     }
 }
